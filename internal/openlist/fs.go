eb990410342219659aaaa90d66d6510ac145fb3f
dean <qicfan@gmail.com>
2026-02-06 19:43:40 +0800

    openlist列表接口将refresh参数设置为true

--- internal/openlist/fs.go
+++ internal/openlist/fs.go
@@ -137,7 +137,7 @@ func (c *Client) FileList(ctx context.Context, path string, page int, perPage in
 		Path:     path,
 		Page:     page,
 		PerPage:  perPage,
-		Refresh:  false,
+		Refresh:  true,
 		Password: "",
 	}
 	result := &Resp[FileListResp]{}
